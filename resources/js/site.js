23b417048dcecfdaaa3dcf871844415724ef4eec
Gunharth Randolf <hello@gunharth.io>
2022-09-01 17:43:11 +0200

    working on nav

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,4 +1,4 @@
-import Alpine from 'alpinejs'
+/* import Alpine from 'alpinejs'
 import collapse from '@alpinejs/collapse'
 import persist from '@alpinejs/persist'
 import focus from '@alpinejs/focus'
@@ -22,3 +22,56 @@ Alpine.plugin(collapse)
 Alpine.plugin(persist)
 Alpine.plugin(focus)
 Alpine.start()
+ */
+
+
+// Grab HTML Elements
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+const hamburger = document.querySelector("#hamburger");
+const logo = document.querySelector("#logo_face");
+
+const classToggle = (el, ...args) => args.map((e) => el.classList.toggle(e));
+
+// Add Event Listeners
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    //classToggle(menu, 'scale-y-0')
+    //classToggle(menu, 'w-0', 'w-full', 'md:w-1/2')
+    //classToggle(menu, 'left-0', '-left-full')
+    //classToggle(menu, '-translate-y-full', 'opacity-0')
+    menu.classList.toggle("-translate-y-full");
+    menu.classList.toggle("opacity-0");
+    //menu.classList.toggle('-translate-x-full')
+    hamburger.classList.toggle("active");
+    logo.classList.toggle("active");
+});
+
+const nav = document.querySelector("nav");
+
+var scrollObject = {};
+window.onscroll = function () {
+/*     scrollObject = {
+        x: window.pageXOffset,
+        y: window.pageYOffset
+     }
+     console.log(scrollObject.y)
+     // If you want to check distance
+     if(scrollObject.y > 200) {
+         // add class
+     } */
+    if (
+        document.body.scrollTop >= 300 ||
+        document.documentElement.scrollTop >= 300
+    ) {
+        nav.classList.add("scrolled")
+        /* nav.classList.add("bg-black");
+        nav.classList.remove("bg-transparent"); */
+        hamburger.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled")
+        /* nav.classList.add("bg-transparent");
+        nav.classList.remove("bg-black"); */
+        hamburger.classList.remove("scrolled");
+    }
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,32 +8,44 @@
 //
 
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  // The various configurable Tailwind configuration files.
-  presets: [
-    require('tailwindcss/defaultConfig'),
-    //require('./tailwind.config.typography.js'),
-    require('./tailwind.config.peak.js'),
-    //require('./tailwind.config.site.js'),
-  ],
-  theme: {
-    screens: {
-      xs: "475px",
-      sm: "640px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-      "3xl": "1920px",
-  },
-  },
-  // Uncomment the next line to enable class based dark mode: https://peak.1902.studio/features/dark-mode.html.
-  // darkMode: 'class',
-  mode: 'jit',
-  // Configure Purge CSS.
-  content: [
-    './resources/views/**/*.html',
-    './resources/js/**/*.js',
-  ],
-  safelist: []
-}
+  content: ["./resources/views/**/*.html", "./resources/js/**/*.js"],
+    // The various configurable Tailwind configuration files.
+    presets: [
+        require("tailwindcss/defaultConfig"),
+        //require('./tailwind.config.typography.js'),
+        //require("./tailwind.config.peak.js"),
+        //require('./tailwind.config.site.js'),
+    ],
+    theme: {
+        screens: {
+            xs: "475px",
+            sm: "640px",
+            md: "768px",
+            lg: "1024px",
+            xl: "1280px",
+            "2xl": "1536px",
+            "3xl": "1920px",
+        },
+        colors: {
+            transparent: "transparent",
+            current: "currentColor",
+            black: colors.black,
+            white: colors.white,
+            brit: {
+                logo: "#231F20",
+                navbackground: "#282123",
+                navhovercolor: "#7e7b7b"
+            },
+        },
+    },
+    // Uncomment the next line to enable class based dark mode: https://peak.1902.studio/features/dark-mode.html.
+    // darkMode: 'class',
+    mode: "jit",
+    plugins: [
+    ],
+    safelist: [],
+};
